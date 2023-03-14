735a0a62b198c4673b1edfd4d3f4c68e300c8896
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-03-14 12:47:32 -0300

    Add files via upload

--- exercioAlu/Program.cs
+++ exercioAlu/Program.cs
@@ -4,9 +4,16 @@ class Program
     static void Main(string[] args)
     {
         Alunos aluno1 = new Alunos();
-        aluno1.nome = "Fulano da Silva";
-        aluno1.nota1 = 4.3;
-        aluno1.nota2 = 7.8;
+        
+        Console.WriteLine("Digite seu nome:");
+        aluno1.nome = Console.ReadLine();
+
+        Console.WriteLine("Digite a primeria nota:");
+        aluno1.nota1 = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Digite a segunda nota:");
+        aluno1.nota2 = Convert.ToDouble(Console.ReadLine());
+        
+        
 
         aluno1.mensagem();
     }
