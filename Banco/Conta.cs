7104a78422f359e656927d536b74e8f1c533ae31
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-03-21 12:39:13 -0300

    Add files via upload

---
+++ Banco/Conta.cs
@@ -0,0 +1,29 @@
+﻿namespace banco;
+class Conta
+{
+   public int Numero {get; set;}
+   private double Saldo{get; set;}
+   public double Limite {get;  set;}
+
+
+   public void Depositar(double valor){
+      this.Saldo +=valor;
+   }
+   
+
+   public double MostrarSaldo(){
+      return this.Saldo + this.Limite;
+   }
+
+   public void AjusteLimite(double valor){
+      this.Limite = valor;
+   }
+
+   public void Sacar(double valor){
+      if(valor > this.Saldo + this.Limite){
+         Console.WriteLine("Você não pode realizar este saque!");
+      }else{
+         this.Saldo -= valor;
+      }
+   }
+}
---
+++ Banco/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace banco;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Banco Etec MCM");
+        Conta c = new Conta();
+        //c.Saldo = 1500;
+        c.Depositar(1500);
+        //c.Limite = 500;
+        c.AjusteLimite(500);
+        c.Sacar(200);
+
+        //Console.WriteLine("Seu saldo é de:" + c.Saldo + "e seu 
+        //limite é de:" + c.Limite);
+        Console.WriteLine("Seu saldo é de:" + c.MostrarSaldo()); 
+    }
+}
