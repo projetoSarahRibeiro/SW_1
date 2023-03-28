ce413921b42fce05b301ec176dc42fccd784716c
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-03-28 12:29:58 -0300

    Add files via upload

---
+++ Projeto_pessoa/Pessoa.cs
@@ -0,0 +1,35 @@
+﻿namespace projeto_pessoa;
+class Pessoa
+{
+    public Pessoa(){
+        this.Nome = "NÃO INFORMADO";
+        this.SobereNome ="NÃO INFORMADO";
+        this.Idade = 0;
+    }
+    public Pessoa( string nome, int idade){
+        this.Nome = nome;
+        this.SobereNome ="NÃO INFORMADO";
+        this.Idade = idade;
+    }
+    
+
+  private string? Nome {get;set;}
+  private string? SobereNome {get;set;}
+  private int Idade {get;set;}
+
+  public void AlterarNome(string nome){
+     this.Nome = nome;
+  }
+  public void AlteraSobreNome(string soberenome){
+     this.Nome = soberenome;
+  }
+  public void AlterarIdade(int idade){
+        int idade1 = idade;
+  }
+  public void MostraDados(){
+    Console.WriteLine("Nome:" + this.Nome);
+    Console.WriteLine("SobreNome:" + this.SobereNome);
+    Console.WriteLine("Idade:" + this.Idade);
+  }
+   
+}
---
+++ Projeto_pessoa/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace projeto_pessoa;
+class Program
+{
+    static void Main(string[] args)
+    {
+       Pessoa p1 = new Pessoa("Godofredo",19);
+       //p1.AlterarNome("Sarah");
+       //p1.AlteraSobreNome("Ribeiro");
+       //p1.AlterarIdade(16);
+       p1.MostraDados();
+    }
+}
