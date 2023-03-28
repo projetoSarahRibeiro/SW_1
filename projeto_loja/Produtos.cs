aba9ecd87508e8552559d8e92f6ac8707ae29069
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-03-28 12:27:11 -0300

    Add files via upload

---
+++ projeto_loja/Produtos.cs
@@ -0,0 +1,32 @@
+﻿namespace projeto_loja;
+class Produtos
+{
+    public Produtos(){
+        this.Nome = "TicToc";
+        this.Preco = 15;
+    }
+    public Produtos(string nome){
+        this.Nome = nome;
+        this.Preco = 10;
+    }
+    public Produtos(string nome, int desconto){
+        this.Nome = nome;
+        this.Preco = 10 - (10*desconto/100);
+    }
+
+
+
+
+    private string? Nome{get;set;}
+    private double Preco {get;set;}
+    public void AlteraNome(string nome){
+        this.Nome = nome;
+    }
+    public void AlteraPreco(double preco){
+        this.Preco = preco;
+    }
+    public void MostraDado(){
+        Console.WriteLine("Nome:" + this.Nome);
+        Console.WriteLine("Preço:" + this.Preco);
+    }
+}
---
+++ projeto_loja/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace projeto_loja;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Produtos p1 = new Produtos();
+        //Produtos p1 = new Produtos("XYZ");
+        Produtos p1 = new Produtos("ABC",10);
+        p1.MostraDado();
+    }
+}
