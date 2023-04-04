7c6e2b43ac6506578e3e61d0840e105eaca1431a
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-04-04 12:16:12 -0300

    Add files via upload

---
+++ Aeroporto/Aeroporto.cs
@@ -0,0 +1,47 @@
+﻿namespace Aeroporto;
+class Aeroporto
+{   
+    public Aeroporto(double codigo, string empresa){
+         this.Codigo = codigo;
+        this.Empresa = empresa;
+        this.CidadeEntra = "Não LIDO";
+        this.CidadeSai = "Não LIDO";
+        
+    }
+    public Aeroporto(double codigo, string empresa, string cidadeentra, string cidadesai){
+        this.Codigo = codigo;
+        this.Empresa = empresa;
+        this.CidadeEntra = cidadeentra;
+        this.CidadeSai = cidadesai ;
+    }
+
+
+    private double Codigo{get;set;}
+    private string? Empresa{get;set;}
+    public string? CidadeEntra{get;set;}
+    public string? CidadeSai{get;set;}
+
+    public void FazCodigo(double codigo){
+          this.Codigo = codigo;
+    }
+    public void FazEmpresa(string empresa){
+          this.Empresa = empresa;
+    }
+    public void Entra(string cidadeentra){
+          this.CidadeEntra = cidadeentra;
+    }
+    public void Sai(string cidadesai){
+          this.CidadeSai = cidadesai;
+    }
+    public void MostraDados(){
+        Console.WriteLine("Código: " + this.Codigo);
+        Console.WriteLine("Companhia: " + this.Empresa);
+        Console.WriteLine("Cidade destino: " + this.CidadeEntra);
+        Console.WriteLine("Cidade de saída: " + this.CidadeSai);
+    }
+
+
+
+
+
+}
---
+++ Aeroporto/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Aeroporto;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Aeroporto a1 = new Aeroporto(122,"Azul","RJ","SP");
+        //a1.FazCodigo(122);
+        //a1.FazEmpresa("Azul");
+        // a1.Entra("RJ");
+        // a1.Sai("SP");
+        a1.MostraDados();
+
+    }
+}
