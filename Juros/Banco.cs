b57ef592fc075942b41364a6decd743378d1aae8
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-05-07 00:23:34 -0300

    Add files via upload

---
+++ Juros/Banco.cs
@@ -0,0 +1,28 @@
+﻿namespace atividadeJuros;
+class BancoBB
+{
+   public string? cliente {get; set;}
+   public double limite {get;set;}
+   public double saldo {get;set;}
+   
+   public double valor;
+   
+   public void deposito (double valor){
+      this.saldo += valor;
+   }
+   public void saque (double valor){
+      this.saldo -= valor;
+   }
+   public double consultaSaldo (){
+      return this.saldo += this.limite;
+   }
+   public double juros(){
+      double Juros = ((valor * 0.06)*12) + valor;
+      return Juros;
+   }
+   public double mes (){
+      double Mes = ((valor * 0.06)+ valor/12);
+      return Mes;
+   }
+    
+}
---
+++ Juros/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace atividadeJuros;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("");
+        Banco banco = new Banco();
+
+        banco.cliente ="Dazai";
+        Console.Write(banco.cliente + " Digite o quanto você quer de emprestimo!");
+        string? textovalor = Console.ReadLine();
+        double valor = Convert.ToDouble(textovalor);
+        banco.valor = valor;
+
+        Console.WriteLine("Seu valor do emprestimo! "+ banco.valor);
+        Console.WriteLine("Seus juros: "+ banco.juros());
+        Console.WriteLine("Juros do mês: "+ banco.mensal());
+
+        banco.saldo = 2000;
+        
+       /* 
+       double saldo = banco.consultaSaldo;
+        banco.limite = 300;
+        banco.deposito(30);
+        banco.deposito(30);
+        banco.limite = 300;
+        Console.WriteLine(banco.consultaSaldo());
+        double saldo = banco.consultaSaldo;
+        
+        /*
+        double saldo = banco.consultaSaldo;
+        banco.limite = 1500;
+        banco.saque();
+        banco.deposito();*/
+
+    }
+}
