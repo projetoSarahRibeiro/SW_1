f1ebaf8dfb9b8e1f57eec3489c21a7369f4307c4
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-05-16 12:43:33 -0300

    Add files via upload

---
+++ Contrato/Contrato.cs
@@ -0,0 +1,7 @@
+﻿namespace Contrato;
+class Contrato
+{
+    public string? Nome {get;set;}
+    public string? Email {get;set;}
+    public string? Telefone {get;set;}
+}
---
+++ Contrato/PessoaFisica.cs
@@ -0,0 +1,10 @@
+﻿namespace Contrato;
+class PessoaFisica
+{
+     public string? Nome {get;set;}
+    public string? Email {get;set;}
+    public string? Telefone {get;set;}
+    public string? Cpf {get;set;}
+    public int Idade {get;set;}
+
+}
