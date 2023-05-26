9f757a71896c897dd88ca065bb7a887a1a45a13a
Sarah Cristina Ribeiro <100208442+projetoSarahRibeiro@users.noreply.github.com>
2023-05-26 16:31:25 -0300

    Add files via upload

---
+++ Ex_1/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace animal;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        Cao c = new Cao();
+        Gato g = new Gato();
+        Humano h = new Humano();
+
+        Console.WriteLine("O homem" + h.Fala() + "O gato" + g.Fala() + " O cão" + c.Fala());
+
+    }
+}
